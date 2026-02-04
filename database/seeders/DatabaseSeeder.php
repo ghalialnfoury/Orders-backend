9ec2c9a18e07ff1cb16746be0113817f39327e25
ghalialnfoury <ghalialnofoury@gmail.com>
2026-02-05 00:56:43 +0300

    Fix syntax for array keys in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,9 @@ public function run(): void
         [
             'name' => 'Admin',
             'email' => 'ghalialnfoury@gmail.com',
-             "phone": "0504795417",
+             "phone"=> "0504795417",
             'password' => bcrypt('123456'),
-            "role": "Admin"
+            "role"=>"Admin"
         ]
     ]);
 
