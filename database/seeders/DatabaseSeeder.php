560f349e5a42cb3c9398b190b457269b951dc716
ghalialnfoury <ghalialnofoury@gmail.com>
2026-02-05 01:13:42 +0300

    Fix user role case in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,21 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
 use Illuminate\Support\Facades\DB;
 
-public function run(): void
+class DatabaseSeeder extends Seeder
 {
-    DB::table('users')->insert([
-        [
-            'name' => 'Admin',
-            'email' => 'ghalialnfoury@gmail.com',
-             "phone"=> "0504795417",
-            'password' => bcrypt('123456'),
-            "role"=>"Admin"
-        ]
-    ]);
-
-   
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin',
+                'email' => 'ghalialnfoury@gmail.com',
+                'phone' => '0504795417',
+                'password' => bcrypt('123456'),
+                'role' => 'admin'
+            ]
+        ]);
+    }
+}
