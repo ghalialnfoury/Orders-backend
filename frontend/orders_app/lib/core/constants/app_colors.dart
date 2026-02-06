bafc685ace6a81077a3b3dfaa86412cf9890e0f7
ghalialnofoury <ghalialnofoury@gmail.com>
2026-02-06 20:17:01 +0300

    remove frontend app

--- frontend/orders_app/lib/core/constants/app_colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const primary = Color(0xFF2563EB);
-  static const background = Color(0xFFF9FAFB);
-  static const text = Color(0xFF111827);
-}
