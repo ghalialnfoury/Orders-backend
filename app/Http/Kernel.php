3b5e6f94dcad70bb2a0de13dd115beb0d877e2ff
ghalialnfoury <ghalialnofoury@gmail.com>
2026-02-12 23:26:22 +0300

    Add CorsForImages middleware to the kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends HttpKernel
         \Illuminate\Http\Middleware\HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+         \App\Http\Middleware\CorsForImages::class,
 
     ];
 
